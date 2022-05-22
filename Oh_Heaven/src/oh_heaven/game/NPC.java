77abe8573a62b63667d2f5f9977a37ab7a90f576
adnanmunshi <adnan.munshi94@gmail.com>
2022-05-23 01:05:25 +1000

    added some stuff

--- Oh_Heaven/src/oh_heaven/game/NPC.java
+++ Oh_Heaven/src/oh_heaven/game/NPC.java
@@ -1,10 +1,48 @@
 package oh_heaven.game;
 
-public class NPC extends Player {
+import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
+import oh_heaven.game.Oh_Heaven.Suit;
 
-	NPC(int id, boolean isNPC) {
+public class NPC extends Player {
+	INPCStrategy npcStrategy;
+	 private Hand hand; 
+	 private Suit lead; 
+	 private Suit trump;
+
+	NPC(int id, boolean isNPC, INPCStrategy npcStrategy) {
 		super(id, isNPC);
+		this.npcStrategy = npcStrategy;
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void placeLead() {
+		npcStrategy.leadDecision(getHand(), getLead(), getTrump());
+	}
+	public void placeFollowing() {
+		npcStrategy.followDecision(getHand(), getLead(), getTrump());
+	}
 
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public Suit getLead() {
+		return lead;
+	}
+
+	public void setLead(Suit lead) {
+		this.lead = lead;
+	}
+	public Suit getTrump() {
+		return trump;
+	}
+
+	public void setTrump(Suit trump) {
+		this.trump = trump;
+	}
 }
